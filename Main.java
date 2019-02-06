d871cf969cbbe5e9e77431c3846e33d7e745f18c
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-02-05 20:08:52 -0500

    Bob and Alice Completed

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+    
+    if(input.equals("Bob")) {
+        System.out.println("Hi Bob!");
+    } else if(input.equals("Alice")) {
+        System.out.println("Hi Alice!");
+    } else {
+        System.out.println("Who goes there?");
+    }
+        
+    
     }
 }
